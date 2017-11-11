059419a2f1202c5d34633071a2dc4c62726977a1
ogcule <ogcule@outlook.com>
2017-11-11 20:44:38 +0000

    update

--- server/dbConnection.js
+++ server/dbConnection.js
@@ -6,6 +6,7 @@ const initOptions = {
 };
 const pgp = require('pg-promise')(initOptions);
 // const connectionString = 'postgres://localhost:5432/services';
+console.log('The database url is :', process.env.DATABASE_URL)
 const connectionString = 'process.env.DATABASE_URL';
 // create database object
 const db = pgp(connectionString);

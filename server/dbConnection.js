ac0f5c4923235536820f4ffb307e8a3b82b4a327
ogcule <ogcule@outlook.com>
2017-11-12 08:10:27 +0000

    update

--- server/dbConnection.js
+++ server/dbConnection.js
@@ -7,7 +7,8 @@ const initOptions = {
 const pgp = require('pg-promise')(initOptions);
 // const connectionString = 'postgres://localhost:5432/services';
 console.log('The database url is :', process.env.DATABASE_URL)
-const connectionString = process.env.DATABASE_URL;
+// when using heroku use their database when using locally use local postgres database
+const connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/services';
 // create database object
 const db = pgp(connectionString);
 
